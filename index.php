22449374631bae8c201fd6b9636885f86270a530
Claire Dune <claire.dune@gmail.com>
2014-09-19 10:55:30 +0200

    Update index.php
    remise a zero pour les étudiants

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 
-    // chargement de la classe rectangle du tp1
-    require_once("source/Rectangle.php");
-    // on construit un rectangle de largeur 200 pixels et de hauteur 300 pixel, 
-    // le constructeur __construct() est appelé
-    $rect = new Rectangle(200,300);
-    // on affiche les paramètres du rectangle, la fonction magique __toSting est appelé 
-    $rect->affichage();
+   echo "<H1>Fichier de test des classes du TP2</H1>"
+    
 ?>
 
